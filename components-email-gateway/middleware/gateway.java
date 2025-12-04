cf3d5c1afc147c164be4bd8e7792b5487183e5fa
Vic3and74toriaFulta3ler <o.verbin762863710@rambler.ru>
2025-12-04 09:44:08 +0800

    add config.less, index.html, ZamaConfig.json

---
+++ components-email-gateway/middleware/gateway.java
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\components-email-gateway\middleware\gateway.java
+// Generated at: 2025-10-05T15:10:27.823Z
---
+++ frontend/web/src/middleware.go
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\middleware.go
+// Generated at: 2025-10-05T15:10:27.831Z
